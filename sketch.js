d66c56b063b5ae91f8b77d6f693cf0e06ff56f01
Glitch (cclab-p5js-template) <none>
2021-03-24 15:55:57 +0000

    🍡🎅 Checkpoint ./sketch.js:29783973/5674

--- sketch.js
+++ sketch.js
@@ -1,40 +1,40 @@
 let particles;
 let detail;
-let x = 0
-let y = 0
-let realx =0
-let realy =0
-let z = 10
-let theta = 0
-let angle = 0
+let x = 0;
+let y = 0;
+let realx = 0;
+let realy = 0;
+let z = 10;
+let theta = 0;
+let angle = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   detail = createSlider(3, 24, 14);
   detail.position(10, height - 30);
-  detail.style('width', '80px');
-	frameRate(30);
+  detail.style("width", "80px");
+  frameRate(30);
 }
 
 function draw() {
-	background(0);
-	saturn = new Planet(50, 50, 50, 40, 1);
-	pluto = new Planet(-150, -100, 20, 25, 2);
-	mars = new Planet(-200, 150, 0, 15, 0);
-	saturn.draw();
-	pluto.draw();
-	mars.draw();
-  push();
-	blackhole1 = new blackhole(50+x, 200+y, 106,1/2);
-	pop();
-	blackhole2 = new blackhole(50, 200, 106,1);
-	blackhole1.draw();
-	blackhole2.draw();
-	theta =frameCount/100
-	x = 400 * cos(theta);
-	y = 200 * sin(theta);
-  pop();
-  }
+  background(0);
+  // saturn = new Planet(50, 50, 50, 40, 1);
+  // pluto = new Planet(-150, -100, 20, 25, 2);
+  // mars = new Planet(-200, 150, 0, 15, 0);
+  // saturn.draw();
+  // pluto.draw();
+  // mars.draw();
+  // push();
+  // blackhole1 = new blackhole(50+x, 200+y, 106,1/2);
+  // pop();
+  // blackhole2 = new blackhole(50, 200, 106,1);
+  // blackhole1.draw();
+  // blackhole2.draw();
+  // theta =frameCount/100
+  // x = 400 * cos(theta);
+  // y = 200 * sin(theta);
+  // pop();
+}
 
 // Plannet class introduction:
 // 1. constructor has 5 arguments
@@ -44,78 +44,79 @@ function draw() {
 // 5. draw() can be used to draw the plannet directly on its position
 
 class Planet {
-	constructor(x, y, z, radius, rings) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
-		this.radius = radius;
-		this.rings = rings;
-	}
-	
-	move(xDist, yDist, zDist) {
-		this.x += xDist;
-		this.y += yDist;
-		this.z += zDist;
-	}
-	
-	getRadius() {
-    return this.radius;
-	}
-	
-	draw() {
-    push();
-		translate(this.x, this.y, this.z);
-		push();
-		noFill();
-		stroke(220);
-		strokeWeight(0.5);
-		rotateY(millis() / 2500);
-		sphere(this.radius, 24, detail.value());
-		noFill();
-		stroke(255);
-		strokeWeight(3);
-		push();
-		rotateX(1.1*(PI/2));
-		if(this.rings >= 1) circle(0, 0, 5 * this.radius * random(0.95, 1.05));
-		pop();
-		push();
-		rotateY(-1.1*(PI/2));
-		if(this.rings >= 2) circle(0, 0, 5 * this.radius * random(0.95, 1.05));
-		pop();
-		pop();
-	}
-}
+  constructor(x, y, z, radius, rings) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.radius = radius;
+    this.rings = rings;
+  }
 
-class blackhole{
-		constructor(x,y,z,size,mass) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
-		this.size = size;
-		this.mass = mass;
-	}
-  
-		draw() {
-			push();
-			this.x +=  5*cos(frameCount / 1);
-			translate(this.x,this.y,this.z)
-			rotateY(millis() / 1000);
-			rotateX(frameCount * 0.01);
-			rotateZ(frameCount * 0.01);
-			push();
-			lights(34,56,128);
-			stroke(10);
-			fill(51,51,51)
-			sphere(100*this.size, 24, 16);
-			pop();
-			rotateY(millis() / 1000);
-			noStroke();
-			let jet =  60*cos(frameCount/100 );
-			fill(random(255),random(255),random(255),90);
-			translate(0,-500*this.size,0)
-			cone(100*this.size, 1000*this.size, 16*this.size, 16*this.size);
-			translate(0,1000*this.size,0)
-			cone(100*this.size, -1000*this.size, 16*this.size, 16*this.size);
-			pop();
-		}
+  move(xDist, yDist, zDist) {
+    this.x += xDist;
+    this.y += yDist;
+    this.z += zDist;
+  }
+
+  getRadius() {
+    return this.radius;
+  }
+
+  draw() {
+    push();
+    translate(this.x, this.y, this.z);
+    push();
+    noFill();
+    stroke(220);
+    strokeWeight(0.5);
+    rotateY(millis() / 2500);
+    sphere(this.radius, 24, detail.value());
+    noFill();
+    stroke(255);
+    strokeWeight(3);
+    push();
+    rotateX(1.1 * (PI / 2));
+    if (this.rings >= 1) {
+      circle(0, 0, 5 * this.radius * random(0.95, 1.05));
+    }
+    pop();
+    push();
+    rotateY(-1.1 * (PI / 2));
+    if (this.rings >= 2) circle(0, 0, 5 * this.radius * random(0.95, 1.05));
+    pop();
+    pop();
+  }
+
+class blackhole {
+  constructor(x, y, z, size, mass) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.size = size;
+    this.mass = mass;
+  }
+
+  draw() {
+    push();
+    this.x += 5 * cos(frameCount / 1);
+    translate(this.x, this.y, this.z);
+    rotateY(millis() / 1000);
+    rotateX(frameCount * 0.01);
+    rotateZ(frameCount * 0.01);
+    push();
+    lights(34, 56, 128);
+    stroke(10);
+    fill(51, 51, 51);
+    sphere(100 * this.size, 24, 16);
+    pop();
+    rotateY(millis() / 1000);
+    noStroke();
+    let jet = 60 * cos(frameCount / 100);
+    fill(random(255), random(255), random(255), 90);
+    translate(0, -500 * this.size, 0);
+    cone(100 * this.size, 1000 * this.size, 16 * this.size, 16 * this.size);
+    translate(0, 1000 * this.size, 0);
+    cone(100 * this.size, -1000 * this.size, 16 * this.size, 16 * this.size);
+    pop();
+  }
 }

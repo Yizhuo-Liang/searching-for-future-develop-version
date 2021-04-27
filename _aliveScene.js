bee791022f8cfc723bf50e1f43a6e2065bfe8563
Glitch (s-earthing-for-future) <none>
2021-04-27 09:21:04 +0000

    Rewound to commit fac803a909c852a79c2b4dd49138cb464daaf268 + 0 edits

--- _aliveScene.js
+++ _aliveScene.js
@@ -1,80 +1,24 @@
-let space_age;
-let spaceship;
-let universe;
-let startPng;
-let earth,
-  planet1,
-  planet2,
-  planet3,
-  planet4,
-  planet5,
-  planet6,
-  planet7,
-  planet8,
-  planet9,
-  planet10;
-let explosde_sound;
-let BGM;
-let narratePoem;
-let victoryBGM;
+function aliveScene() {
+  background(0);
+  moveAround();
+  ship1.draw(camX, camY, camZ - 350, 15, tiltZ, tiltX, spaceship);
+  if (frameCount === 1 || frameCount % 180 == 0) {
+    generatePlanet(ship1);
+    // console.log(planets.length);
+  }
 
+  drawPlanets();
+  getWarningLevel(ship1, planets);
+  sb.draw(
+    ship1.getLocation().x + _W / 4,
+    ship1.getLocation().y - _H / 4,
+    ship1.getLocation().z
+  );
 
-function preload() {
-  space_age = loadFont(
-    "https://cdn.glitch.com/48b3940f-dc59-484b-bb22-aaa9c4991ca3%2FNasa.ttf?v=1617322691693"
-  );
-  spaceship = loadModel("assets/spaceship2.obj");
-  universe = loadImage(
-    "https://cdn.glitch.com/48b3940f-dc59-484b-bb22-aaa9c4991ca3%2Funiverse-background-1.jpg?v=1617194401240"
-  );
-  startPng = loadImage(
-    "https://cdn.glitch.com/48b3940f-dc59-484b-bb22-aaa9c4991ca3%2FNew%20Project.png?v=1617291457112"
-  );
-  earth = loadImage(
-    "https://cdn.glitch.com/48b3940f-dc59-484b-bb22-aaa9c4991ca3%2Fearth.jpg?v=1616633286407"
-  );
-  planet1 = loadImage(
-    "https://cdn.glitch.com/48b3940f-dc59-484b-bb22-aaa9c4991ca3%2Fplanet2.jpg?v=1616633287289"
-  );
-  planet2 = loadImage(
-    "https://cdn.glitch.com/48b3940f-dc59-484b-bb22-aaa9c4991ca3%2Fplanet2.jpg?v=1616633287289"
-  );
-  planet3 = loadImage(
-    "https://cdn.glitch.com/48b3940f-dc59-484b-bb22-aaa9c4991ca3%2Fplanet3.jpg?v=1616633288086"
-  );
-  planet4 = loadImage(
-    "https://cdn.glitch.com/48b3940f-dc59-484b-bb22-aaa9c4991ca3%2Fplanet4.jpg?v=1616633289270"
-  );
-  planet5 = loadImage(
-    "https://cdn.glitch.com/48b3940f-dc59-484b-bb22-aaa9c4991ca3%2Fplanet5.png?v=1616633291018"
-  );
-  planet6 = loadImage(
-    "https://cdn.glitch.com/48b3940f-dc59-484b-bb22-aaa9c4991ca3%2Fplanet6.jfif?v=1617298849359"
-  );
-  planet7 = loadImage(
-    "https://cdn.glitch.com/48b3940f-dc59-484b-bb22-aaa9c4991ca3%2Fplanet7.jfif?v=1617298851576"
-  );
-  planet8 = loadImage(
-    "https://cdn.glitch.com/48b3940f-dc59-484b-bb22-aaa9c4991ca3%2Fplanet8.jpg?v=1617298854353"
-  );
-  planet9 = loadImage(
-    "https://cdn.glitch.com/48b3940f-dc59-484b-bb22-aaa9c4991ca3%2Fplanet9.jpg?v=1617298856293"
-  );
-  planet10 = loadImage(
-    "https://cdn.glitch.com/48b3940f-dc59-484b-bb22-aaa9c4991ca3%2Fplanet10.jpg?v=1617298858415"
-  );
-  soundFormats("mp3", "ogg");
-  explosde_sound = loadSound(
-    "https://cdn.glitch.com/48b3940f-dc59-484b-bb22-aaa9c4991ca3%2F11369.mp3?v=1617032492745"
-  );
-  BGM = loadSound(
-    "https://cdn.glitch.com/48b3940f-dc59-484b-bb22-aaa9c4991ca3%2FBGM.mp3?v=1617047619315"
-  );
-  narratePoem = loadSound(
-    "https://cdn.glitch.com/48b3940f-dc59-484b-bb22-aaa9c4991ca3%2Fmedia-deee5997.mp3?v=1617183652881"
-  );
-  victoryBGM = loadSound(
-    "https://cdn.glitch.com/48b3940f-dc59-484b-bb22-aaa9c4991ca3%2FStar%20Wars%20Main%20Theme%20(Full).mp3?v=1617319295463"
+  displayPoem.draw(
+    ship1.getLocation().x,
+    ship1.getLocation().y + _H / 4,
+    ship1.getLocation().z
   );
 }
 
